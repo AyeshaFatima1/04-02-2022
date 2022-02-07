56fea64ba10e84e77cfe83a7fe52cdae247e4c57
ayeshafatima <ayeshafatima@192.168.0.102>
2022-02-07 13:18:03 +0530

    Example1 java file added in to the project 04-02-2022

---
+++ 04-02-2022/src/com/javaDatatypes/Example1.java
@@ -0,0 +1,18 @@
+package com.javaDatatypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//Write a java program to print customer information(id,name,and address)
+		
+		int customerid=1243;
+		String customername="Ayesha";
+		String customeraddress="Hyderabad";
+		
+		System.out.println(customerid);
+		System.out.println(customername);
+		System.out.println(customeraddress);
+		
+	}
+
+}
